af9514fe632769ac40c910a1c459316a81dabce3
cristopxer <cristopherortiz457@gmail.com>
2023-10-20 15:00:46 -0600

    Add exercise1 is_a_palidrome and it's unit testing

---
+++ exercise1.py
@@ -0,0 +1,21 @@
+# A palindrome, also called palindrome, palindrome, palindrome or palindrome, is a word, or phrase, that is
+# phrase that reads the same in one direction as in the other. Example ANA can be read backwards and forwards and means the same thing.
+# backwards and means the same thing.
+
+def is_palindrome(word):
+    """Checks if a word is a palindrome.
+
+    Args:
+      word: A string.
+
+    Returns:
+      True if the word is a palindrome, False otherwise.
+    """
+
+    if not isinstance(word, str):
+        return 'You must pass a string'
+
+    word = word.lower().replace(" ", "")
+
+    # Check if the word is the same forwards and backwards.
+    return word == word[::-1]
---
+++ tests/test_exercise1.py
@@ -0,0 +1,14 @@
+from exercises.exercise1 import is_palindrome
+
+# test if a word is a palindrome
+def test_is_palindrome() -> None:
+    assert is_palindrome("ana")
+
+# test if the word pass as arg is not a palindrome
+def test_is_not_palindrome() -> None:
+    assert is_palindrome("juan") == False
+
+# test if the word pass as arg is not a str
+def test_is_palindrome_int() -> None:
+    assert is_palindrome(123) == 'You must pass a string'
+
